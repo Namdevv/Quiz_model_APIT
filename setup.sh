c6c86aef8b1d338420e05d54db7241280fb087ac
Namtran0912 <namtran34311@gmail.com>
2025-09-09 13:07:46 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # ==== Config nhẹ ====
 VENV_DIR="$HOME/venv"          # Đường dẫn venv
 PROJECT_DIR="$HOME"            # Thư mục làm việc mặc định
-USE_GPU="auto"                 # "auto" | "yes" | "no"
 
 echo "==> Updating apt..."
 sudo apt update && sudo apt upgrade -y
@@ -24,21 +23,6 @@ source "$VENV_DIR/bin/activate"
 
 echo "==> Upgrading pip/setuptools/wheel..."
 python -m pip install --upgrade pip setuptools wheel
-
-echo "==> Installing Jupyter (Notebook + Lab) inside venv..."
-pip install notebook jupyterlab
-
-echo "==> Installing core ML deps..."
-pip install "datasets>=3.4.1,<4.0.0" "huggingface_hub>=0.34.0" hf_transfer sentencepiece protobuf
-
-pip3 install torch torchvision --index-url https://download.pytorch.org/hl/cu126
-
-echo "==> Installing LLM training stack..."
-pip install xformers==0.0.32.post2
-pip install --no-deps bitsandbytes accelerate peft trl triton cut_cross_entropy unsloth_zoo
-pip install unsloth
-pip install "transformers==4.55.4"
-pip install gradio matplotlib seaborn scikit-learn
 
 echo
 echo "✅ Setup hoàn tất!"
